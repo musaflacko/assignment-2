663756c083ba3a6ce0fca84e11d9a8b416388341
Musa Flacko <s10194189@connect.np.su.sg>
2021-01-02 19:37:27 +0800

    Template

---
+++ script.js
@@ -0,0 +1,30 @@
+/*let url = "https://api.data.gov.sg/v1/transport/taxi-availability";
+
+fetch(url)
+.then(response => response.json()) 
+.then(function(data){
+  console.log(data);
+});
+
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);*/
+
+const successCallback = (position) => {
+  console.log(position);
+  var latitude = GeolocationCoordinates[4];
+  var longitude = GeolocationCoordinates[5];
+};
+
+const errorCallback = (error) => {
+  console.error(error);
+};
+
+navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 1.3521, lng: 103.8198 },
+    zoom: 15,
+  });
+}
