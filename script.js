393ee235775e105c0a47e996f6d1599f431b1fac
Musa Flacko <s10194189@connect.np.su.sg>
2021-01-03 16:37:25 +0800

    Add Markers

--- script.js
+++ script.js
@@ -11,8 +11,25 @@ navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 let map;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 1.3521, lng: 103.8198 },
-    zoom: 15,
+  var options = {
+    center: { lat: 1.3339, lng: 103.7220 },
+    zoom: 15
+  }
+
+  var map = new
+  google.maps.Map(document.getElementById('map'), options);
+
+  var marker = new google.maps.Marker({
+    position: { lat: 1.3521, lng: 103.8198},
+    map: map
   });
+
+  addMarker({ lat:1.3339, lng: 103.7220 });
+
+  function addMarker(coords){
+    var marker = new google.maps.Marker({
+      position : coords,
+      map : map
+    });
+  }
 }
