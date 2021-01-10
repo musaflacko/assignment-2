f5be7c152faa39657a8ecb7f942e444bae652693
Musa Flacko <s10194189@connect.np.su.sg>
2021-01-11 01:00:17 +0800

    Add Directions

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
-const successCallback = (position) => {
-  console.log(position);
-};
-
-const errorCallback = (error) => {
-  console.error(error);
-};
-
-navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-
-let map;
-
 function initMap() {
-  var options = {
-    center: { lat: 1.3339, lng: 103.7220 },
-    zoom: 15
-  }
-
-  var map = new
-  google.maps.Map(document.getElementById('map'), options);
-
-  var marker = new google.maps.Marker({
-    position: { lat: 1.3521, lng: 103.8198},
-    map: map
+  const directionsRenderer = new google.maps.DirectionsRenderer();
+  const directionsService = new google.maps.DirectionsService();
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 12,
+    center: { lat: 1.3521, lng: 103.8198 },
   });
+  directionsRenderer.setMap(map);
+  directionsRenderer.setPanel(document.getElementById("right-panel"));
+  const control = document.getElementById("floating-panel");
+  control.style.display = "block";
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
 
-  addMarker({ lat:1.3339, lng: 103.7220 });
+  const onChangeHandler = function () {
+    calculateAndDisplayRoute(directionsService, directionsRenderer);
+  };
+  document.getElementById("start").addEventListener("change", onChangeHandler);
+  document.getElementById("end").addEventListener("change", onChangeHandler);
+}
 
-  function addMarker(coords){
-    var marker = new google.maps.Marker({
-      position : coords,
-      map : map
-    });
-  }
+function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+  const start = document.getElementById("start").value;
+  const end = document.getElementById("end").value;
+  directionsService.route(
+    {
+      origin: start,
+      destination: end,
+      travelMode: google.maps.TravelMode.DRIVING,
+    },
+    (response, status) => {
+      if (status === "OK") {
+        directionsRenderer.setDirections(response);
+      } else {
+        window.alert("Directions request failed due to " + status);
+      }
+    }
+  );
 }
